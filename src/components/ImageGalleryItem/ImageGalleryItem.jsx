9e633efbdf03b51a5d0a12c9006985e6cf2367d4
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-05-15 22:04:32 +0300

    fixed bugs

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import s from './ImageGalleryItem.module.css';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import s from './ImageGalleryItem.module.css';
+// import PropTypes from 'prop-types';
 
-export const ImageGalleryItem = ({handleImageClick, largeImageURL}) => {
+// export const ImageGalleryItem = ({handleImageClick, largeImageURL}) => {
 
-    return (
-      <li className={s.galleryItem}>
-        <img onClick={()=> handleImageClick(largeImageURL)} src={this.props.smallImgURL} alt={this.props.id} />
-      </li>
-    );
-  }
+//     return (
+//       <li className={s.galleryItem}>
+//         <img onClick={()=> handleImageClick(largeImageURL)} src={this.props.smallImgURL} alt={this.props.id} />
+//       </li>
+//     );
+//   }
   
 
 
 
-ImageGalleryItem.propTypes = {
-  id: PropTypes.number.isRequired,
-  smallImgURL: PropTypes.string.isRequired,
-  handleImageClick: PropTypes.func.isRequired,
-  largeImageURL: PropTypes.string.isRequired,
-  };
+// ImageGalleryItem.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   smallImgURL: PropTypes.string.isRequired,
+//   handleImageClick: PropTypes.func.isRequired,
+//   largeImageURL: PropTypes.string.isRequired,
+//   };
 
+import React from 'react';
+import s from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
+
+export const ImageGalleryItem = ({ handleImageClick, smallImgURL, largeImageURL }) => {
+  return (
+    <li className={s.galleryItem}>
+      <img onClick={() => handleImageClick(largeImageURL)} src={smallImgURL} alt={largeImageURL} />
+    </li>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  smallImgURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  handleImageClick: PropTypes.func.isRequired,
+};
