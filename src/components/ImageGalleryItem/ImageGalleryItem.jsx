0adf59665ca8083301e027d8a95161e72d10e11c
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-05-15 22:10:11 +0300

    fixed bugs

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,40 +1,20 @@
-// import React from 'react';
-// import s from './ImageGalleryItem.module.css';
-// import PropTypes from 'prop-types';
-
-// export const ImageGalleryItem = ({handleImageClick, largeImageURL}) => {
-
-//     return (
-//       <li className={s.galleryItem}>
-//         <img onClick={()=> handleImageClick(largeImageURL)} src={this.props.smallImgURL} alt={this.props.id} />
-//       </li>
-//     );
-//   }
-  
-
-
-
-// ImageGalleryItem.propTypes = {
-//   id: PropTypes.number.isRequired,
-//   smallImgURL: PropTypes.string.isRequired,
-//   handleImageClick: PropTypes.func.isRequired,
-//   largeImageURL: PropTypes.string.isRequired,
-//   };
-
 import React from 'react';
 import s from './ImageGalleryItem.module.css';
 import PropTypes from 'prop-types';
 
-export const ImageGalleryItem = ({ handleImageClick, smallImgURL, largeImageURL }) => {
-  return (
-    <li className={s.galleryItem}>
-      <img onClick={() => handleImageClick(largeImageURL)} src={smallImgURL} alt={largeImageURL} />
-    </li>
-  );
-};
+export const ImageGalleryItem = ({handleImageClick, largeImageURL, smallImgURL, id}) => {
+
+    return (
+      <li className={s.galleryItem}>
+        <img onClick={()=> handleImageClick(largeImageURL)} src={smallImgURL} alt={id} />
+      </li>
+    );
+  }
+
 
 ImageGalleryItem.propTypes = {
+  id: PropTypes.number.isRequired,
   smallImgURL: PropTypes.string.isRequired,
-  largeImageURL: PropTypes.string.isRequired,
   handleImageClick: PropTypes.func.isRequired,
-};
+  largeImageURL: PropTypes.string.isRequired,
+  };
