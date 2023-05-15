bec74a447ee5d5def22069fb06f6a7b3ceffbbfc
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-05-15 20:08:32 +0300

    fixed bugs mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,171 @@
+// import React, { Component } from 'react';
+
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// import Searchbar from './Searchbar/Searchbar';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Loader from './Loader/Loader';
+// import Button from './Button/Button';
+
+// import { fetchImg } from './Servises/api';
+
+// export default class App extends Component {
+//   state = {
+//     URL: 'https://pixabay.com/api/',
+//     API_KEY: '34966777-a1579da70d4a26e0e4c8e2fcd',
+//     pictures: [],
+//     error: '',
+//     status: 'idle',
+//     page: 1,
+//     query: '',
+//     totalHits: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       this.state.query !== prevState.query || this.state.page !== prevState.page)
+//     {
+//       this.setState({ status: 'pending', pictures: [], page: 1 });
+//       fetchImg(
+//         this.state.URL,
+//         this.state.API_KEY,
+//         this.state.query,
+//         this.state.page
+//       )
+//         .then((pictures) => {
+//           if (!pictures.total) {
+//             toast.error('Did find anything, mate');
+//             return
+//           }
+//           const selectedProperties = pictures.hits.map(
+//             ({ id, largeImageURL, webformatURL }) => {
+//               return { id, largeImageURL, webformatURL };
+//             }
+//           );
+//           this.setState((prevState) => {
+//             return {
+//               pictures: [...prevState.pictures, ...selectedProperties],
+//               status: 'resolved',
+//               totalHits: pictures.total,
+//             };
+//           });
+//         })
+//         .catch((error) => this.setState({ error, status: 'rejected' }));
+//     }
+//   }
+
+//   processSubmit = (query) => {
+//     this.setState({ query });
+//   };
+
+//   handleLoadMore = () => {
+//     this.setState((prevState) => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   render() {
+//     const { pictures, status, totalHits } = this.state;
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.processSubmit} />
+//         {pictures.length > 0 && <ImageGallery images={pictures} />}
+//         {totalHits > pictures.length && (
+//           <Button onClick={this.handleLoadMore} />
+//         )}
+//         {status === 'pending' && <Loader />}
+//         <ToastContainer autoClose={2000} />
+//       </>
+//     );
+//   }
+// }
+
+// новий код
+
+// import React, { Component } from 'react';
+
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// import Searchbar from './Searchbar/Searchbar';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Loader from './Loader/Loader';
+// import Button from './Button/Button';
+
+// import { fetchImg } from './Servises/api';
+
+// export default class App extends Component {
+// state = {
+// URL: 'https://pixabay.com/api/',
+// API_KEY: '34966777-a1579da70d4a26e0e4c8e2fcd',
+// pictures: [],
+// error: '',
+// status: 'idle',
+// page: 1,
+// query: '',
+// shovBtn: false,
+// };
+
+// componentDidUpdate(prevProps, prevState) {
+// if (
+// this.state.query !== prevState.query || this.state.page !== prevState.page
+// ) {
+// this.setState({ status: 'pending' });
+// fetchImg(
+// this.state.URL,
+// this.state.API_KEY,
+// this.state.query,
+// this.state.page
+// )
+// .then((pictures) => {
+// if (!pictures.total) {
+//   toast.error('Did find anything, mate');
+//   return
+// }
+// const selectedProperties = pictures.hits.map(
+// ({ id, largeImageURL, webformatURL }) => {
+// return { id, largeImageURL, webformatURL };
+// }
+// );
+// this.setState((prevState) => {
+// return {
+// pictures: [...prevState.pictures, ...selectedProperties],
+// status: 'resolved',
+// shovBtn: this.state.page < [pictures.totalHits / 12]
+// };
+// });
+// })
+// .catch((error) => this.setState({ error, status: 'rejected' }));
+// }
+// }
+
+// processSubmit = (query) => {
+// this.setState({ query, pictures: [], page: 1, shovBtn: true });
+// };
+
+// handleLoadMore = () => {
+// this.setState((prevState) => {
+// return { page: prevState.page + 1 };
+// });
+// };
+
+// render() {
+// const { pictures, status, shovBtn } = this.state;
+// return (
+// <>
+// <Searchbar onSubmit={this.processSubmit} />
+// {pictures.length > 0 && <ImageGallery images={pictures} />}
+// {shovBtn && <Button onClick={this.handleLoadMore} />}
+// {status === 'pending' && <Loader />}
+// <ToastContainer autoClose={2000} />
+// </>
+// );
+// }
+// }
+
+// новий код 3
+
 import React, { Component } from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -11,72 +179,89 @@ import Button from './Button/Button';
 import { fetchImg } from './Servises/api';
 
 export default class App extends Component {
-  state = {
-    URL: 'https://pixabay.com/api/',
-    API_KEY: '34966777-a1579da70d4a26e0e4c8e2fcd',
-    pictures: [],
-    error: '',
-    status: 'idle',
-    page: 1,
-    query: '',
-    totalHits: null,
-  };
+state = {
+URL: 'https://pixabay.com/api/',
+API_KEY: '34966777-a1579da70d4a26e0e4c8e2fcd',
+pictures: [],
+error: '',
+status: 'idle',
+page: 1,
+query: '',
+shovBtn: false,
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.query !== prevState.query ||
-      (this.state.query === prevState.query && this.state.page !== prevState.page)
-    ) {
-      this.setState({ status: 'pending', pictures: [], page: 1 });
-      fetchImg(
-        this.state.URL,
-        this.state.API_KEY,
-        this.state.query,
-        this.state.page
-      )
-        .then((pictures) => {
-          if (!pictures.total) {
-            toast.error('Did find anything, mate');
-          }
-          const selectedProperties = pictures.hits.map(
-            ({ id, largeImageURL, webformatURL }) => {
-              return { id, largeImageURL, webformatURL };
-            }
-          );
-          this.setState((prevState) => {
-            return {
-              pictures: [...prevState.pictures, ...selectedProperties],
-              status: 'resolved',
-              totalHits: pictures.total,
-            };
-          });
-        })
-        .catch((error) => this.setState({ error, status: 'rejected' }));
-    }
-  }
+componentDidUpdate(prevProps, prevState) {
+if (
+this.state.query !== prevState.query || this.state.page !== prevState.page
+) {
+this.setState({ status: 'pending' });
+fetchImg(
+this.state.URL,
+this.state.API_KEY,
+this.state.query,
+this.state.page
+)
+.then((pictures) => {
+if (!pictures.total) {
+  toast.error('Did find anything, mate');
+  return
+}
+const selectedProperties = pictures.hits.map(
+({ id, largeImageURL, webformatURL }) => {
+return { id, largeImageURL, webformatURL };
+}
+);
+this.setState((prevState) => {
+return {
+pictures: [...prevState.pictures, ...selectedProperties],
+status: 'resolved',
+shovBtn: this.state.page < Math.ceil(pictures.totalHits / 12),
+};
+});
+})
+.catch((error) => this.setState({ error, status: 'rejected' }));
+}
+}
 
-  processSubmit = (query) => {
-    this.setState({ query });
-  };
+processSubmit = (query) => {
+  this.setState({ query, pictures: [], page: 1, shovBtn: true }, () => {
+    fetchImg(this.state.URL, this.state.API_KEY, this.state.query, this.state.page)
+      .then((pictures) => {
+        if (!pictures.total) {
+          toast.error('Did not find anything, mate');
+          return;
+        }
+        const selectedProperties = pictures.hits.map(({ id, largeImageURL, webformatURL }) => {
+          return { id, largeImageURL, webformatURL };
+        });
+        this.setState((prevState) => {
+          return {
+            pictures: [...prevState.pictures, ...selectedProperties],
+            status: 'resolved',
+            shovBtn: this.state.page < Math.ceil(pictures.totalHits / 12),
+          };
+        });
+      })
+      .catch((error) => this.setState({ error, status: 'rejected' }));
+  });
+};
 
-  handleLoadMore = () => {
-    this.setState((prevState) => {
-      return { page: prevState.page + 1 };
-    });
-  };
+handleLoadMore = () => {
+this.setState((prevState) => {
+return { page: prevState.page + 1 };
+});
+};
 
-  render() {
-    const { pictures, status, totalHits } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.processSubmit} />
-        {pictures.length > 0 && <ImageGallery images={pictures} />}
-        {totalHits > pictures.length && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-        {status === 'pending' && <Loader />}
-        <ToastContainer autoClose={2000} />
-      </>
-    );
-  }
+render() {
+const { pictures, status, shovBtn } = this.state;
+return (
+<>
+<Searchbar onSubmit={this.processSubmit} />
+{pictures.length > 0 && <ImageGallery images={pictures} />}
+{shovBtn && <Button onClick={this.handleLoadMore} />}
+{status === 'pending' && <Loader />}
+<ToastContainer autoClose={2000} />
+</>
+);
+}
 }
